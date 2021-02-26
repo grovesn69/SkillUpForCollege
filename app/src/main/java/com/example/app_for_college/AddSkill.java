6af8044d5ee9467bd302275ff57ed4bc4d980a76
Ciara Byrne <byrnec66@tcd.ie>
2021-02-26 14:34:25 +0000

    DataBase_implemented
    database linked. Names are recorded along with skills and corresponding metrics

--- app/src/main/java/com/example/app_for_college/AddSkill.java
+++ app/src/main/java/com/example/app_for_college/AddSkill.java
@@ -1,5 +1,16 @@
 package com.example.app_for_college;
 
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,12 +27,27 @@ public class AddSkill extends AppCompatActivity {
         setContentView(R.layout.activity_add_skill);
         setTitle("Add Skill");
         Intent i = getIntent();
+
+
     }
 
     public void GoToHome(View v){
         Intent i = new Intent(this, GoToHome.class);
-        String message = ((EditText)findViewById(R.id.SkillNameText)).getText().toString();
-        i.putExtra("SkillKey", message);
+        String message2 = ((EditText)findViewById(R.id.SkillNameText)).getText().toString();
+        i.putExtra("SkillKey", message2);
         startActivity(i);
+
+        String MetricForY = ((EditText)findViewById(R.id.MetricForY)).getText().toString();
+        String MetricForX = ((EditText)findViewById(R.id.MetricForX)).getText().toString();
+        //add a skill to the data base
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("/skills") ;
+
+        DatabaseReference newIssueRef = rootRef.push();
+       //newIssueRef.setValue(message2);
+        DatabaseReference metric2Ref = FirebaseDatabase.getInstance().getReference("/skills" + "/" + message2);
+        metric2Ref.child("metrics").push().setValue(MetricForY);
+        metric2Ref.child("metrics").push().setValue(MetricForX);
+
+
     }
 }
--- app/src/main/java/com/example/app_for_college/SkillData.java
+++ app/src/main/java/com/example/app_for_college/SkillData.java
@@ -4,9 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class SkillData extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +37,24 @@ public class SkillData extends AppCompatActivity {
         setContentView(R.layout.activity_skill_data);
         Intent i = getIntent();
         String message = i.getStringExtra("Pass");
-        ((TextView)findViewById(R.id.SkillTextView)).setText(message);
-    }
 
+        ((TextView) findViewById(R.id.SkillTextView)).setText(message);
+    }
 }
+    /*
+    public void datalist() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("/DbId/skills");
+
+        //String userId;
+        mDatabase.child("skills").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(Objects.requireNonNull(task.getResult()).getValue()));
+                }
+            }
+        });
+
+    }*/
