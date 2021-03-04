f9ba88f257a08e012a974762c12d78ba52fe97fa
grovesn69 <60435819+grovesn69@users.noreply.github.com>
2021-03-04 10:58:09 +0000

    Dynamic Button partly working

--- app/src/main/java/com/example/app_for_college/GoToHome.java
+++ app/src/main/java/com/example/app_for_college/GoToHome.java
@@ -3,30 +3,37 @@ package com.example.app_for_college;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GoToHome extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_to_home);
         setTitle("Home");
+
         Intent i = getIntent();
         String message = i.getStringExtra("SkillKey");
-        ((TextView)findViewById(R.id.SkillTextOne)).setText(message);
+        Button myButton = new Button(this);
+        myButton.setText(message);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.buttonlayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        ll.addView(myButton,lp);
     }
+
     public void AddSkill(View v){
         Intent i = new Intent(this, AddSkill.class);
         startActivity(i);
     }
-    public void SkillData(View v){
+    public void SkillData(View v) {
         Intent in = new Intent(this, SkillData.class);
-        String message = ((EditText)findViewById(R.id.SkillTextOne)).getText().toString();
+        String message = ((EditText) findViewById(R.id.SkillTextOne)).getText().toString();
         in.putExtra("Pass", message);
         startActivity(in);
     }
+
 }
